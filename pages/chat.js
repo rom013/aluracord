61eeed090b74737b98bd2aa6049d136f37cbfb03
Romullo <romullomelo0130@gmail.com>
2022-02-03 07:52:35 -0300

    delete implantado

--- pages/chat.js
+++ pages/chat.js
@@ -1,23 +1,23 @@
 //https://github.com/supabase/supabase-js
 
-import {Helmet} from "react-helmet";
+import { Helmet } from "react-helmet";
 import { Box, Text, TextField, Image, Button, Icon } from '@skynexui/components';
 import React from 'react';
 import appConfig from '../config.json';
 import { createClient } from '@supabase/supabase-js';
 import { useRouter } from 'next/router'
 import { ButtonSendSticker } from "../src/components/ButtonSendSticker";
-
+import { format } from 'date-fns' //yarn add date-fns
 
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MzMzNDA5MCwiZXhwIjoxOTU4OTEwMDkwfQ.sQ38gxReoWChyVEvJF291dNEfeXB5v8CRvU14P5i51Y"
 const SUPABASE_URL = "https://lvuwmzfcykkzjgxdwent.supabase.co"
 const supabesClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
 
 //ativar o real time do supabase
-function escutaMensagem(addMsg){
+function escutaMensagem(addMsg) {
     return supabesClient
         .from('mensagens')
-        .on('INSERT', (resLive)=>{
+        .on('INSERT', (resLive) => {
             // console.log("houve uma nova mensagem", resLive.new)
             addMsg(resLive.new)
         })
@@ -34,40 +34,40 @@ export default function ChatPage() {
     const roteamento = useRouter()
     const usuarioLogado = roteamento.query.username
 
-// So chama este useEffect na primeira vez que a pagina carrega
-    
-    React.useEffect(()=>{
+    // So chama este useEffect na primeira vez que a pagina carrega
+
+    React.useEffect(() => {
         supabesClient
             .from('mensagens')
             .select('*')
-            .order('id', {ascending: false})
-            .then(({ data, status })=>{
+            .order('id', { ascending: false })
+            .then(({ data, status }) => {
                 console.log(data, status)
                 setLoading(false)
                 setMsgList(data)
             })
-            escutaMensagem((novaMsg)=>{
-                console.log(novaMsg)
-                setMsgList((valorAtual)=>{
-                    return [
-                        novaMsg,
-                        ...valorAtual
-                    ]
-                })
+        escutaMensagem((novaMsg) => {
+            console.log(novaMsg)
+            setMsgList((valorAtual) => {
+                return [
+                    novaMsg,
+                    ...valorAtual
+                ]
             })
+        })
     }, [])
 
-    function hendleNewMsg(msgTextfield){
+    function hendleNewMsg(msgTextfield) {
         const msg = {
             by: usuarioLogado,
             text: msgTextfield,
-            data: new Date()
+            data: format(new Date(), 'dd/MM/yyyy - HH:mm')
         }
 
         supabesClient
             .from('mensagens')
             .insert(msg)
-            .then(({ data })=>{
+            .then(({ data }) => {
                 console.log("a")
             })
         setMensagem('')
@@ -76,9 +76,9 @@ export default function ChatPage() {
     return (
         <>
             <Helmet>
-                    <meta charSet="utf-8" />
-                    <title>Aluracord - chat</title>
-                    <link rel="shortcut icon" href="https://cdn.discordapp.com/attachments/691421631700271114/935320323379843192/logo-alura.png" />
+                <meta charSet="utf-8" />
+                <title>Aluracord - chat</title>
+                <link rel="shortcut icon" href="https://cdn.discordapp.com/attachments/691421631700271114/935320323379843192/logo-alura.png" />
             </Helmet>
             <Box
                 styleSheet={{
@@ -114,12 +114,12 @@ export default function ChatPage() {
                             flexDirection: 'column',
                             borderRadius: '5px',
                             padding: '16px',
-                            
+
                         }}
                     >
-                        {loading ? <Loading/> : <MessageList mensagens={msgList} />}
-                        
-                        
+                        {loading ? <Loading /> : <MessageList mensagens={msgList} batata={setMsgList}/>}
+
+
                         {/* {mensagem} */}
                         <Box
                             as="form"
@@ -129,15 +129,15 @@ export default function ChatPage() {
                             }}
                         >
                             <TextField
-                                value = {mensagem}
-                                onChange={function (event){
+                                value={mensagem}
+                                onChange={function (event) {
                                     setMensagem(event.target.value)
                                 }}
-                                onKeyPress={function(event){
+                                onKeyPress={function (event) {
                                     // console.log(event)
-                                    if(event.code == "Enter"){
+                                    if (event.code == "Enter") {
                                         event.preventDefault()
-                                        if(mensagem.length >= 1){
+                                        if (mensagem.length >= 1) {
                                             event.preventDefault()
                                             hendleNewMsg(mensagem)
                                         }
@@ -156,7 +156,7 @@ export default function ChatPage() {
                                     color: appConfig.theme.colors.neutrals[200],
                                 }}
                             />
-                            <Button 
+                            <Button
                                 buttonColors={{
                                     contrastColor: appConfig.theme.colors.neutrals["000"],
                                     mainColor: appConfig.theme.colors.primary[500],
@@ -167,22 +167,22 @@ export default function ChatPage() {
                                     height: "100%",
                                     display: {
                                         sm: "none",
-                                        xs: "flex"  
+                                        xs: "flex"
                                     }
                                 }}
-                                iconName="arrowRight" 
+                                iconName="arrowRight"
 
-                                onClick={()=>{
-                                    if(mensagem.length >= 1){
+                                onClick={() => {
+                                    if (mensagem.length >= 1) {
                                         hendleNewMsg(mensagem)
                                     }
-                                    
+
                                 }}
                             />
 
                             {/* CallBack */}
                             <ButtonSendSticker
-                                onStickerClick={(sticker)=>{
+                                onStickerClick={(sticker) => {
                                     console.log("enviar pro banco")
                                     hendleNewMsg(`:sticker: ${sticker}`)
                                 }}
@@ -207,6 +207,14 @@ function Header() {
                     as="form"
                     variant='tertiary'
                     colorVariant='neutral'
+                    styleSheet={{
+                        hover:
+                        {
+                            color: "#fa3b3b",
+                            backgroundColor: "rgb(157 67 67 / 80%)"
+                        }
+
+                    }}
                     label='Logout'
                     href="/"
                 />
@@ -214,8 +222,8 @@ function Header() {
         </>
     )
 }
-function Loading(){
-    return(
+function Loading() {
+    return (
         <Box
             styleSheet={{
                 display: 'flex',
@@ -277,9 +285,26 @@ function Loading(){
         </Box>
     )
 }
+function api(mensagens) {
+    console.log("olha ", mensagens)
+    return mensagens
+}
 function MessageList(props) {
     //console.log(props.mensagens);
-    
+
+    function deleteMessage(messageId) {
+        const newMessageList = props.mensagens.filter((message) => {
+            return messageId != message.id
+        })
+        supabesClient
+            .from('mensagens')
+            .delete()
+            .match({id: messageId})
+            .then((res)=>{
+                console.log("ok")})
+        return props.batata(newMessageList)
+    }
+
     return (
         <Box
             tag="ul"
@@ -292,7 +317,8 @@ function MessageList(props) {
                 overflow: 'auto',
             }}
         >
-            {props.mensagens.map((mensagens)=>{
+            {props.mensagens.map((mensagens) => {
+
                 return (
                     <Text
                         key={mensagens.id}
@@ -311,34 +337,90 @@ function MessageList(props) {
                                 marginBottom: '8px',
                             }}
                         >
-                            <Image
-                                styleSheet={{
-                                    width: '20px',
-                                    height: '20px',
-                                    borderRadius: '50%',
-                                    display: 'inline-block',
-                                    marginRight: '8px',
-                                }}
-                                src={`https://github.com/${mensagens.by}.png`}
-                            />
-                            <Text tag="strong">
-                                {mensagens.by}
-                            </Text>
-                            <Text
-                                styleSheet={{
-                                    fontSize: '10px',
-                                    marginLeft: '8px',
-                                    color: appConfig.theme.colors.neutrals[300],
-                                }}
-                                tag="span"
+
+                            <div
+                            // styleSheet={{
+                            //     display: 'inline-block'
+                            // }}
                             >
-                                {mensagens.data}
-                                {/* {(new Date().toLocaleDateString())} */}
-                            </Text>
+                                <img
+                                    src={`https://github.com/${mensagens.by}.png`}
+                                />
+
+
+
+                                {/* <PerfilUser/> */}
+                                <Text tag="strong"
+                                    styleSheet={{
+                                        marginLeft:"10px"
+                                    }}
+                                >
+                                    {mensagens.by}
+                                </Text>
+                                <Text
+                                    styleSheet={{
+                                        fontSize: '10px',
+                                        marginLeft: '8px',
+                                        color: appConfig.theme.colors.neutrals[300],
+                                    }}
+                                    tag="span"
+                                >
+                                    {mensagens.data}
+                                    {/* {(new Date().toLocaleDateString())} */}
+                                </Text>
+                                <span>
+                                <Button
+                                    variant="tertiary"
+                                    iconName="trash"
+                                    colorVariant="neutral"
+                                    onClick={(e)=>{
+                                        e.preventDefault()
+                                        deleteMessage(mensagens.id)}}
+                                    styleSheet={{
+                                        position: 'absolute',
+                                        top: '10px',
+                                        right: '20px',
+                                        height: '1%',
+                                        width: '2%',
+                                        color: appConfig.theme.colors.neutrals[400],
+                                        hover: {
+                                            backgroundColor: 'transparent'
+                                        }
+                                    }}
+                               />
+                                </span>
+                            </div>
+                            <style jsx>{`
+                                img{
+                                    width: 20px;
+                                    height: 20px;
+                                    border-radius: 50%
+                                }
+                                div{
+                                    margin-right: 5px;
+                                    cursor: pointer;
+                                    display: flex;
+                                    margin-right: 5px;
+                                    cursor: pointer;
+                                    width: 100%;
+                                    justify-content: flex-start;
+                                    align-items: center
+                                }
+                                div:hover span{
+                                    display: flex
+                                }
+                                span{
+                                    display: none;
+                                    margin-left: 70px;
+                                    position: relative;
+                                    margin-top: -70px;
+                                }
+
+                            `}</style>
                         </Box>
-                        {}
+                        { }
                         {/* {mensagens.text.startsWith(':sticker:').toString()} */}
-                       {mensagens.text.startsWith(':sticker:') ? <Image src={mensagens.text.replace(':sticker:', '')}/> : mensagens.text}
+                        {mensagens.text.startsWith(':sticker:') ? <Image src={mensagens.text.replace(':sticker:', '')} /> : mensagens.text}
 
                     </Text>
                 )
@@ -351,7 +433,7 @@ function MessageList(props) {
 
 
 
-/* 
+/*
     /==================================================\
 
     onChange - evento que executa quando há alguma alteração
@@ -364,3 +446,11 @@ function MessageList(props) {
     verdade ? console.log("verdade") : console.log("falso")
     //falso
 */
+
+
+//https://github.com/Taylor-2T9/aluracord-matrix/blob/main/pages/chat.js
+//https://github.com/bpcosta2003/aluracord/tree/master/pages
+
+/*fetch(`https://api.github.com/users/${mensagens}`)
+        .then((valueApi)=>{return valueApi.json()})
+        .then((valueJson)=>{console.log(valueJson.followers)})s */
